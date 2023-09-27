1e5a7ca4f36f5210be42f1db83823f5c9f65b922
RanjaniAnandan <ranjanianandan2001@gmail.com>
2023-09-27 15:59:14 +0530

    Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Header.css';
-import { Search } from '@material-ui/icons';
-import { ShoppingBasket } from '@material-ui/icons';
-import { useStateValue } from './StateProvider';
-import { auth } from './firebase';
-import Maskqueen1 from "./Maskqueen1.png"
-
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+import { Search } from "@material-ui/icons";
+import { ShoppingBasket } from "@material-ui/icons";
+import { useStateValue } from "./StateProvider";
+import { auth } from "./firebase";
 
 function Header() {
   const [{ basket, user }] = useStateValue();
@@ -15,74 +13,67 @@ function Header() {
     if (user) {
       auth.signOut();
     }
-  }
+  };
 
   return (
     <nav className="header">
-        {/* logo on the left -> img */}
-        
-        <Link to="/">
-          <img 
-            className="header__logo" 
-            src={Maskqueen1}
-            alt="logo" 
-          />
+      {/* logo on the left -> img */}
+
+      <Link to="/" className="header__link">
+        <p className="header__logo">ANRA</p>
+      </Link>
+
+      {/* Search box */}
+      <div className="header__search">
+        <input type="text" className="header__searchInput" />
+        <Search className="header__searchIcon" />
+      </div>
+
+      {/* 3 Links */}
+
+      <div className="header__nav">
+        {/* 1st link */}
+        <Link to={!user && "/LoginPage"} className="header__link">
+          <div onClick={login} className="header__option">
+            <span className="header__optionLineOne">Hello {user?.email}</span>
+            <span className="header__optionLineTwo">
+              {user ? "Sign Out" : "Sign In"}{" "}
+            </span>
+          </div>
         </Link>
 
-        {/* Search box */}
-        <div className="header__search">
-          <input type="text" className="header__searchInput" />
-          <Search className="header__searchIcon" />
-        </div>
-
-        {/* 3 Links */}
-
-        <div className="header__nav">
-          {/* 1st link */}
-          <Link to= {!user && "/LoginPage"} className='header__link'>
-          <div onClick={login} className='header__option'>
-            <span className='header__optionLineOne'>Hello {user?.email}</span>
-            <span className='header__optionLineTwo'>{user ? 'Sign Out' : 'Sign In'} </span>
+        {/* 2nd link */}
+        <Link to="/" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Returns</span>
+            <span className="header__optionLineTwo"> &Orders</span>
           </div>
-          </Link>
+        </Link>
 
-          {/* 2nd link */}
-          <Link to="/" className='header__link'>
-          <div className='header__option'>
-            <span  className='header__optionLineOne'>Returns</span>
-            <span  className='header__optionLineTwo'> &Orders</span>
+        {/* 3rd link */}
+        <Link to="/" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Your</span>
+            <span className="header__optionLineTwo">Prime</span>
           </div>
-          </Link>
+        </Link>
 
-
-          {/* 3rd link */}
-          <Link to="/" className='header__link'>
-          <div className='header__option'>
-            <span  className='header__optionLineOne'>Your</span>
-            <span  className='header__optionLineTwo'>Prime</span>
+        {/* 4th link */}
+        <Link to="/Checkout" className="header__link">
+          <div className="header__optionBasket">
+            {/* Shopping basket icon */}
+            <ShoppingBasket />
+            {/* Number of items in the basket */}
+            <span className="header__optionLineTwo header__basketCount">
+              {basket?.length}
+            </span>
           </div>
-          </Link>
+        </Link>
+      </div>
 
-
-          {/* 4th link */}
-          <Link to="/Checkout" className='header__link'>
-            <div className='header__optionBasket'>
-              {/* Shopping basket icon */}
-              <ShoppingBasket />
-              {/* Number of items in the basket */}
-              <span className='header__optionLineTwo header__basketCount'>
-                {basket?.length}
-              </span>
-            </div>
-          
-          </Link>
-
-        </div>
-
-
-        {/* Basket icon with number */}
+      {/* Basket icon with number */}
     </nav>
-  )
+  );
 }
 
 export default Header;
--- src/Login.js
+++ src/Login.js
@@ -1,71 +1,77 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import "./Login.css";
 import { Link, useNavigate } from "react-router-dom";
-import { auth }  from './firebase';
-import Maskqueen from "./Maskqueen.png"
-
+import { auth } from "./firebase";
+import ANRA from "./ANRA.png";
 
 function Login() {
-    const navigate = useNavigate();
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    
-    const login = (event) => {
-        event.preventDefault();  //this stops the refresh!!!
-        // do the login logic...
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-      auth
+  const login = (event) => {
+    event.preventDefault(); //this stops the refresh!!!
+    // do the login logic...
+
+    auth
       .signInWithEmailAndPassword(email, password)
       .then((auth) => {
         // logged In, redirect to homepage...
         navigate("/");
       })
       .catch((e) => alert(e.message));
-    };
+  };
 
-    const register = (event) => {
-        event.preventDefault();  //this stops the refresh!!!
-        // do the login logic...
+  const register = (event) => {
+    event.preventDefault(); //this stops the refresh!!!
+    // do the login logic...
 
-      auth
+    auth
       .createUserWithEmailAndPassword(email, password)
       .then((auth) => {
         // created a user and logged in, redirect to homepage
         navigate("/");
       })
       .catch((e) => alert(e.message));
-    };
-
+  };
 
   return (
     <div className="login">
       <Link to="/">
-        <img
-          className="login__logo"
-          src={Maskqueen} 
-          alt="logo"
-        />
+        <img className="login__logo" src={ANRA} alt="logo" />
       </Link>
 
       <div className="login__container">
         <h1>Sign in</h1>
         <form>
-            <h5>E-mail</h5>
-            <input value={email} onChange = {e => setEmail(e.target.value)} type="email" />
-            <h5>Password</h5>
-            <input value={password} onChange = {e => setPassword(e.target.value)} type="password" />
-            <button onClick={login} type="submit" className="login__signInButton">Sign In</button>
+          <h5>E-mail</h5>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+          />
+          <h5>Password</h5>
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+          />
+          <button onClick={login} type="submit" className="login__signInButton">
+            Sign In
+          </button>
         </form>
 
         <p>
-            By signing-in you agree to MaskQueen Conditions of Use & Sale. Please see our Privacy Notice, our Cookies Notice and our Interest. Based Ads Notice.
+          By signing-in you agree to Anra Conditions of Use & Sale. Please see
+          our Privacy Notice, our Cookies Notice and our Interest. Based Ads
+          Notice.
         </p>
-        <button onClick={register} className="login__registerButton">Create your MaskQueen Account</button>
+        <button onClick={register} className="login__registerButton">
+          Create your Anra Account
+        </button>
       </div>
-
-      
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
